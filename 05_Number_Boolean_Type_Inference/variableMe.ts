a40a3fb766bcaeb2b12ac53b16c3cf1d3c073aea
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-18 08:23:30 +0600

    adding some more notes in the text file

--- 05_Number_Boolean_Type_Inference/variableMe.ts
+++ 05_Number_Boolean_Type_Inference/variableMe.ts
@@ -1,8 +1,18 @@
 let greetings: string = "Hello Hitesh";
 console.log(greetings);
 
-let mynum = 6;
+// let mynum = 6;
 
 // greetings.toLowerCase()
+
+// Number
+
+let userID: number /*(this number declaration is called infer)*/ = 23423.3
+
+
+//Boolean
+
+let isLoggedin: boolean /* (declaring the type of the variable is called infer) */= false;
+
 
 export{} 
