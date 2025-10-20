fe744856af15329817c3c011e0bd277b8e642616
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-20 07:27:53 +0600

    writing and running command line

--- 07_Do_you_really_know_functions/pr_chapter07.js
+++ 07_Do_you_really_know_functions/pr_chapter07.js
@@ -6,5 +6,10 @@ function addTwo(num) {
 function getUpper(val) {
     return val.toUpperCase();
 }
+function signUpUser(name, email, isPaid) {
+    console.log("User name is ".concat(name, ". User email is ").concat(email, ". User have ").concat(isPaid, " the subscription"));
+}
 addTwo(5);
 getUpper("Mahin");
+signUpUser("Mahin", "asdlfjksdf234@gmail.com", "paid");
+signUpUser(1, 2, 3);
--- 07_Do_you_really_know_functions/pr_chapter07.ts
+++ 07_Do_you_really_know_functions/pr_chapter07.ts
@@ -6,12 +6,13 @@ function getUpper(val: string) {
   return val.toUpperCase();
 }
 
-function signUpUser(name, email, password) {
-
+function signUpUser(name, email, isPaid) {
+    console.log(`User name is ${name}. User email is ${email}. User have ${isPaid} the subscription`)
 }
 
 addTwo(5);
 getUpper("Mahin");
+signUpUser("Mahin","asdlfjksdf234@gmail.com","paid")
 
 
 signUpUser(1,2,3)
