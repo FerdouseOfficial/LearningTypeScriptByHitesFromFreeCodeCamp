9b60b050f7d62f57a140da768014271393ad8aa8
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-22 19:24:14 +0600

    start writing code

--- 09_Bad_behavior_of_objects/ch09.ts
+++ 09_Bad_behavior_of_objects/ch09.ts
@@ -1,3 +1,3 @@
 //  Learning the concept of Bad behavior of Objects
 
-// ALthough ts is a superscript of the js. Its also have some bad habits. its also have some misbehave. May be in future it will go away. But its still exist here.
+// ALthough ts is a superscript of the js. Its also have some bad habits. its also have some misbehave. May be in future it will go away. But its still exist here. 
