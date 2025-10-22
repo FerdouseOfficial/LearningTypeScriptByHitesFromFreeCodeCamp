f1bd01d6d7ef899ddd1ff9cfb42e8637acc599ee
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-22 06:59:27 +0600

    writing the same code writen in the chapter 07

--- 08_A_better_way_to_write_function/script.ts
+++ 08_A_better_way_to_write_function/script.ts
@@ -1,1 +1,24 @@
 // Chapter 08: A better way to write function
+function addTwo(num: number) {
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: true) {
+  console.log(
+    `User name is ${name}. User email is ${email}. User have ${isPaid} the subscription`
+  );
+}
+
+let logInUser = (user: string, password: number | string) => {
+  console.log(`Username : ${user}, Password: ${password}`);
+};
+
+addTwo(5);
+getUpper("Mahin");
+
+signUpUser("mahin", "rohitsharmafuchka", true);
+logInUser("Romio23", "234******asdfkj2");
