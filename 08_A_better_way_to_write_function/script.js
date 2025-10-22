d545298bed816f77299a1a27218262add11ac48d
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-22 07:10:25 +0600

    compiling ts into js

--- 08_A_better_way_to_write_function/script.js
+++ 08_A_better_way_to_write_function/script.js
@@ -1,17 +1,33 @@
-// Chapter 08: A better way to write function
-function addTwo(num) {
-    return num + 2;
-}
-function getUpper(val) {
-    return val.toUpperCase();
-}
-function signUpUser(name, email, isPaid) {
-    console.log("User name is ".concat(name, ". User email is ").concat(email, ". User have ").concat(isPaid, " the subscription"));
-}
-var logInUser = function (user, password) {
-    console.log("Username : ".concat(user, ", Password: ").concat(password));
-};
-addTwo(5);
-getUpper("Mahin");
-signUpUser("mahin", "rohitsharmafuchka", true);
-logInUser("Romio23", "234******asdfkj2");
+// // Chapter 08: A better way to write function
+// function addTwo(num: number) {
+//   return num + 2;
+// }
+// function getUpper(val: string) {
+//   return val.toUpperCase();
+// }
+// function signUpUser(name: string, email: string, isPaid: true) {
+//   console.log(
+//     `User name is ${name}. User email is ${email}. User have ${isPaid} the subscription`
+//   );
+// }
+// let logInUser = (user: string, password: (number | string)) => {
+//   console.log(`Username : ${user}, Password: ${password}`);
+// };
+// addTwo(5);
+// getUpper("Mahin");
+// signUpUser("mahin", "rohitsharmafuchka", true);
+// logInUser("Romio23", "234******asdfkj2");
+// Edge Cases
+// ------ 01 -------
+// function getValue(myVal: number): boolean {
+//     if(myVal > 5) {
+//         return true;
+//     }
+//     return "200 ok"
+// }
+// ------------------------- 02 ------------------------
+// const getHello = (s: string): string => {
+//   return "";
+// };
+//    --------------------- 03 ------------------
+var heros = ["thor", "spiderman", "ironman"];
